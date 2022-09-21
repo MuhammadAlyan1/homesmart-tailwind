2acddff83b6ade9aae390b7d1d30859410b34efe
Muhammad Alyan <alyan0332@gmail.com>
2022-09-21 22:44:30 +0500

    add data

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const featuresData = [
+  {
+    id: 'feature1',
+    image: '',
+    title: 'Safe',
+    description: 'Our products are secure and private out-of-the-box',
+  },
+  {
+    id: 'feature2',
+    image: '',
+    title: 'Efficient',
+    description: 'Feel good about your wallet and the environment',
+  },
+  {
+    id: 'feature3',
+    image: '',
+    title: 'Proven',
+    description: 'Leading the Smart Home world for 10 years',
+  },
+];
